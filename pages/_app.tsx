d98b5d6860210c9ba262804d6154f60dc5227440
arosyihuddin <arosyihuddin6@gmail.com>
2024-08-06 22:50:27 +0700

    fix _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,44 +1,15 @@
-// import type { AppProps } from "next/app";
-// import { AuthProvider } from "@saas-ui/auth";
-// import { SaasProvider } from "@saas-ui/react";
-// import { Layout } from "components/layout";
-// import theme from "../theme";
-
-// function MyApp({ Component, pageProps }: AppProps) {
-//   const { announcement, header, footer } = pageProps;
-
-//   return (
-//     <SaasProvider theme={theme}>
-//       <AuthProvider>
-//         <Layout
-//           announcementProps={announcement}
-//           headerProps={header}
-//           footerProps={footer}
-//         >
-//           <Component {...pageProps} />
-//         </Layout>
-//       </AuthProvider>
-//     </SaasProvider>
-//   );
-// }
-
-// export default MyApp;
-
-
-// Originall
 import type { AppProps } from "next/app";
 import { AuthProvider } from "@saas-ui/auth";
 import { SaasProvider } from "@saas-ui/react";
 import { Layout } from "components/layout";
 import theme from "../theme";
-import { auth } from "../utils/firebaseClient"; // Import firebase auth
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { announcement, header, footer } = pageProps;
 
   return (
     <SaasProvider theme={theme}>
-      <AuthProvider auth={auth}>
+      <AuthProvider>
         <Layout
           announcementProps={announcement}
           headerProps={header}
